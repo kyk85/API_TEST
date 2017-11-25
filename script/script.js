b64d819161a7360aac4fc5804814b840e7894834
YK <kay2kay1@gmail.com>
2017-11-25 16:45:54 +0800

    test

--- script/script.js
+++ script/script.js
@@ -6,11 +6,14 @@ $(function(){
 			url:"https://quiet-citadel-87837.herokuapp.com/restaurants",
 			success: function(data){
 				console.log(data);
-				$("#rating").text(data[0].reviews[0].rating);
+				//$("#rating").text(data[0].reviews[0].rating);
 				$("#restaurantName").html(data[0].name+"<br>"+data[0].address);
 				$("#openingHours").html(data[0].opening[0].days+"<br>"+data[0].opening[0].hours);
 				//$("#facilities").text(data[0].reviews[0].rating);
 				//$("#userReviews").text(data[0].reviews[0].rating);
+				for (i=0;i<data[0].reviews[0].rating;i++){
+					$("#rating").html(+"<span class="glyphicon glyphicon-star"></span>");
+				}
 
 			}
 			
