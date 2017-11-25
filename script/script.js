b78850751d2b9609582106910b0424c3ae207082
YK <kay2kay1@gmail.com>
2017-11-25 15:35:17 +0800

    test

--- script/script.js
+++ script/script.js
@@ -8,7 +8,7 @@ $(function(){
 				console.log(data);
 				$("#rating").text(data[0].reviews[0].rating);
 				$("#restaurantName").html(data[0].name+"<br>"+data[0].address);
-				$("#openingHours").html(data[0].opening[0].days+"<br>"+data[0].opening[0].hours;
+				$("#openingHours").html(data[0].opening[0].days+"<br>"+data[0].opening[0].hours);
 				//$("#facilities").text(data[0].reviews[0].rating);
 				//$("#userReviews").text(data[0].reviews[0].rating);
 
